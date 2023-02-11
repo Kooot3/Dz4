13cbb8ae694d6a500d9995b53b0620a15f5ec9ad
Kooot3 <124166824+Kooot3@users.noreply.github.com>
2023-02-11 15:25:57 +0300

    Add HandGun and Crosshair

---
+++ Assets/Scripts/HandGun.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class HandGun: MonoBehaviour
+
+    {
+    [SerializeField] private float force = 4;
+    [SerializeField] private GameObject impactPrefab;
+    [SerializeField] private Transform shootPoint;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(shootPoint.position, shootPoint.forward, out var hit))
+            {
+                print(hit.transform.gameObject.name);
+
+                Instantiate(impactPrefab, hit.point, Quaternion.LookRotation(hit.normal, Vector3.up));
+
+                var rigidbody = hit.transform.GetComponent<Rigidbody>();
+                if (rigidbody == null)
+                {
+                    return;
+                }
+
+                rigidbody.AddForce(shootPoint.forward * force, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        
+        Gizmos.DrawRay(shootPoint.position, shootPoint.forward * 9999);
+    }
+    }
+}
